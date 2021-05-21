195233be5ec4245fab35058acaafeb510c71b0fe
Anwesh43 <anweshthecool0@gmail.com>
2021-05-22 02:49:56 +0530

    adding WindowLineBallView class

--- windowlineballview/src/main/java/com/example/windowlineballview/WindowLineBalView.kt
+++ windowlineballview/src/main/java/com/example/windowlineballview/WindowLineBalView.kt
@@ -65,3 +65,19 @@ fun Canvas.drawWLBNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawWindowLineBall(scale, w, h, paint)
 }
+
+class WindowLineBallView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
