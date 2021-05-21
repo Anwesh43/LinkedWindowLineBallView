26069989bebb9acb3ada8f2f8c09d74850c0964b
Anwesh43 <anweshthecool0@gmail.com>
2021-05-22 02:16:17 +0530

    adding constants and libraries

--- windowlineballview/src/main/java/com/example/windowlineballview/WindowLineBalView.kt
+++ windowlineballview/src/main/java/com/example/windowlineballview/WindowLineBalView.kt
@@ -1,2 +1,29 @@
 package com.example.windowlineballview
 
+import android.view.View
+import android.view.MotionEvent
+import android.graphics.Paint
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.RectF
+import android.content.Context
+import android.app.Activity
+
+val colors : Array<Int> = arrayOf(
+    "#f44336",
+    "#311B92",
+    "#00C853",
+    "#00C853",
+    "#C51162"
+).map {
+    Color.parseColor(it)
+}.toTypedArray()
+val parts : Int = 4
+val scGap : Float = 0.02f / parts
+val strokeFactor : Float = 90f
+val sizeFactor : Float = 3.9f
+val delay : Long = 20
+val backColor : Int = Color.parseColor("#BDBDBD")
+val rFactor : Float = 12.9f
+val deg : Float = 45f
+
