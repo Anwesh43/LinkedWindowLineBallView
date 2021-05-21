7e095176757527fdedcfff2d41ba8f2ec441ee60
Anwesh43 <anweshthecool0@gmail.com>
2021-05-22 05:15:11 +0530

    adding view in activity

--- app/src/main/java/com/example/linkedwindowlineballview/MainActivity.kt
+++ app/src/main/java/com/example/linkedwindowlineballview/MainActivity.kt
@@ -2,10 +2,18 @@ package com.example.linkedwindowlineballview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
+import com.example.windowlineballview.WindowLineBallView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        WindowLineBallView.create(this)
+        fullScreen()
     }
+}
+
+fun MainActivity.fullScreen() {
+    supportActionBar?.hide()
+    window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
 }
